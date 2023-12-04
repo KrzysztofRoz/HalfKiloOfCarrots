6c6a04c8758c1edc5077c05d6303ec396361212f
Fiol <radoslawfolcik@gmail.com>
2023-12-04 20:26:15 +0100

    add temporary slugs json, check html parser on larger recipes count (not working)

--- RecipePuller/src/main/java/com/halfkiloofcarrots/recipepuller/service/aniagotyje/AniaRecipeHtmlParser.java
+++ RecipePuller/src/main/java/com/halfkiloofcarrots/recipepuller/service/aniagotyje/AniaRecipeHtmlParser.java
@@ -6,6 +6,7 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
 import java.util.List;
@@ -13,6 +14,7 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
+@Service
 public class AniaRecipeHtmlParser {
 
     public RecipeData parse(String html) {
--- RecipePuller/src/main/java/com/halfkiloofcarrots/recipepuller/service/aniagotyje/AniaScheduler.java
+++ RecipePuller/src/main/java/com/halfkiloofcarrots/recipepuller/service/aniagotyje/AniaScheduler.java
@@ -1,6 +1,9 @@
 package com.halfkiloofcarrots.recipepuller.service.aniagotyje;
 
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.halfkiloofcarrots.recipepuller.model.dto.RecipeData;
 import com.halfkiloofcarrots.recipepuller.model.dto.TagDto;
 import com.halfkiloofcarrots.recipepuller.model.dto.TagListDto;
 import lombok.RequiredArgsConstructor;
@@ -12,8 +15,12 @@ import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -21,38 +28,66 @@ import java.util.List;
 public class AniaScheduler {
 
     private final AniaProxy proxy;
+    private final AniaRecipeHtmlParser parser;
+    private ObjectMapper objectMapper = new ObjectMapper();
 
     //  TODO
     // map to Json String list -> zapisać do pliku i nie strzelać za każdym razem
 
     // strzał po przepisy
-    @Scheduled(fixedDelay = 10000)
-    public void fetchRecipe() throws IOException {
+//    @Scheduled(fixedDelay = 10000)
+    public void fetchSlugs() throws IOException {
 //        RecipeDataDTO obj =proxy.getRecipe("zeberka-w-kapuscie");
 //        log.info("Data: {}", obj);
         Document aniaTag = Jsoup.parse(new URL("https://aniagotuje.pl/"), 20000);
         List<String> tagsListAnia = AniaTagsHtmlParser.parseTags(aniaTag.html());
-        List<List<TagDto>> listOfTags = tagsListAnia
+        Set<String> listOfTags = tagsListAnia
                 .stream()
                 .map(tag -> fetchSlugs(tag))
-                .toList();
-        log.info("Tag list: {}", listOfTags);
+                .flatMap(List::stream)
+                .map(TagDto::slug)
+                .collect(Collectors.toSet());
+        String slugsJson = objectMapper.writeValueAsString(listOfTags);
+        log.info("Tag list: {}", slugsJson);
+    }
+
+    @Scheduled(fixedDelay = 10000)
+    public void fetchUpdatedRecipes() throws IOException {
+        String slugsString = new String(Files.readAllBytes(Paths.get("C:\\Projects\\HalfKiloOfCarrots\\HalfKiloOfCarrots\\RecipePuller\\src\\main\\resources\\SlugsJson.json")));
+        Set<String> recipes = objectMapper.readValue(slugsString, new TypeReference<Set<String>>() {
+        });
+//        log.info("" + recipes);
+        List<RecipeData> allRecipes = getAllRecipes(recipes);
+        String recipesJson = objectMapper.writeValueAsString(allRecipes);
+        //TODO fix methodology - make it optional (breaks on kotleciki-z-sezamem-i-mizuna)
+        log.info("" + recipesJson);
     }
 
     private List<TagDto> fetchSlugs(String tag) {
         Integer pageNumber = 0;
         List<TagDto> tagsList = new ArrayList<>();
-        while (true) {
+        boolean isLast = false;
+        while (!isLast) {
             TagListDto dto = proxy.getRecipeByTag(tag, pageNumber++);
             tagsList.addAll(dto.content());
             log.info("Current page slug: {} ", dto.content());
             sleep();
-
-            if (dto.last()) {
-                break;
-            }
+            isLast = dto.last();
         }
         return tagsList;
+    }
+
+    private List<RecipeData> getAllRecipes(Set<String> slugsSet) {
+        int setLimiter = 10;
+        return slugsSet
+                .stream()
+                .limit(setLimiter)
+                .map(s -> {
+                    sleep();
+                    return proxy.getRecipe(s);
+                })
+                .map(dto -> parser.parse(dto.body()))
+                .toList();
     }
 
     private static void sleep() {
